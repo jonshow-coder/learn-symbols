97ec05bd42cb71777b489f6f5f29aea08ce558c3
abdulsalam jamaea <abdulsalam.jamaea@gmail.com>
2019-06-26 21:25:42 +0200

    finish symbol and i think i understand it

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ const person = {
 	age: 18,
 	[idSymbol]: 1000
 }
+// some use to symbol
+const RUN = Symbol.for("run");
+
+function letItRun(run) {
+	if (run === RUN) {
+		console.log("it works");
+	} else {
+		console.log("it doesn't");
+	}
+}
+// it won't work
+letItRun("run");
+// it will work because Symbol.for("run") is identical to RUN
+letItRun(Symbol.for("run"))
+// but if i do this 
+// Symbol("run") === Symbol("run") --> false
